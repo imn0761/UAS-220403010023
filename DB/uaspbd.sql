dceb7af89137ec9a691067db4cbfb18be0f73c4d
imn0761 <imn0761@gmail.com>
2023-12-25 21:13:21 +0700

    add database

---
+++ DB/uaspbd.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 25 Des 2023 pada 13.03
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uaspbd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `login`
+--
+-- Kesalahan membaca struktur untuk tabel uaspbd.login: #1932 - Table &#039;uaspbd.login&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel uaspbd.login: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `uaspbd`.`login`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pemesanan`
+--
+-- Kesalahan membaca struktur untuk tabel uaspbd.pemesanan: #1932 - Table &#039;uaspbd.pemesanan&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel uaspbd.pemesanan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `uaspbd`.`pemesanan`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- proses_pemesanan.php
+++ proses_pemesanan.php
@@ -1,4 +1,3 @@
-
 <?php
     include 'connection.php';
     if (isset($_POST['submit'])){
@@ -6,8 +5,11 @@
         $tanggal_checkin = $_POST['tanggal_checkin'];
         $jumlah_hari = $_POST['jumlah_hari'];
         $tipe_kamar = $_POST['tipe_kamar'];
-        $sql="INSERT INTO pemesanan (nama_cust, tanggal_checkin, jumlah_hari, tipe_kamar) VALUES
-        ('$nama_cust', '$tanggal_checkin', '$jumlah_hari', '$tipe_kamar')";
+        // $sql="INSERT INTO pemesanan (nama_cust, tanggal_checkin, jumlah_hari, tipe_kamar) VALUES
+        // ('$nama_cust', '$tanggal_checkin', '$jumlah_hari', '$tipe_kamar')";
+
+        $sql= "INSERT INTO pemesanan (`id_pemesanan`, `nama_cust`, `tanggal_checkin`, `jumlah_hari`, `tipe_kamar`)
+        VALUES ('','$nama_cust', '$tanggal_checkin', '$jumlah_hari', '$tipe_kamar')";
 
     if(mysqli_query($conn, $sql)){
         header('location: pemesanan.php');
@@ -17,4 +19,3 @@
     mysqli_close($conn);
     }
 ?>
-
